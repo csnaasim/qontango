cd1502ee05bd333cc45133508c982c49b65149db
asim.khan@csnainc.com <csna123AK!>
2022-01-04 12:36:35 +0500

    adding authorize keyword to client controller

--- RoleUserApi/Controllers/ClientController.cs
+++ RoleUserApi/Controllers/ClientController.cs
@@ -6,9 +6,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Authorization;
 namespace RoleUserApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ClientController : ControllerBase
